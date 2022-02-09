5f5e94ac29415d5e36ea8d4102fa8e79025b53a5
rakesh3899 <rakeshmistry3899@gmail.com>
2022-02-10 02:08:37 +0600

    Comments

---
+++ 2.Comments.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is a comment
+echo '<h1>Comments</h1>';
+
+/*
+This is a comment
+You can use comments to write notes about your code
+This is a very good practice
+*/
+echo "Yoho, I did it!";
