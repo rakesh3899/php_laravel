5cca2c5b63796cc1471774a49d8e7f210e54ef86
rakesh3899 <rakeshmistry3899@gmail.com>
2022-02-10 02:22:16 +0600

    Variable

---
+++ 3.Variable.php
@@ -0,0 +1,15 @@
+<?php
+
+// c or c++ variable
+$num = 5;
+$num1 = 6;
+
+echo $num + $num1;
+
+echo "</br>";
+// string concatenation
+$name1 = "Hello";
+
+$name2 = "World";
+
+echo $name1 . " - " . $name2;
