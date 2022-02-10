52fd78ecc77ab513c25452ccf03cc86f36682183
rakesh3899 <rakeshmistry3899@gmail.com>
2022-02-11 02:02:04 +0600

    Echo/Output

---
+++ 4.echo.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Hello World!";
+echo ("Another line");
+
+echo "</br>";
+$status = print "Hello World!";
+if ($status) {
+    echo "Printing Successful";
+} else {
+    echo "Printing Failed";
+}
